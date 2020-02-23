b5e8955fd509c5bbb11bd0c452fd922cd0a838b4
pthuencher <paul.thuencher@gmail.com>
2020-02-23 16:35:17 +0100

    Refactor image processing

--- src/actions.py
+++ src/actions.py
@@ -9,31 +9,38 @@ import src.helpers as helpers
 from src.helpers import *
 
 
+def __process_image(img, args):
+    # 1. crop image
+    if args.crop:
+        dx = args.crop[0]
+        dy = args.crop[1]
+        img = img[dx[0]:dx[1], dy[0]:dy[1]]
+
+    # 2. transform image
+    # TODO: implement e.g. pan, tilt, ...
+
+    # 3. resize image
+    if args.resize:
+        img = cv2.resize(img, args.resize, interpolation = cv2.INTER_LINEAR)
+
+    return img
 
 # Calculate preview image and show
 def do_preview(source, args):
     # Read preview image
     preview = source.get_preview()
-
-    # crop image
-    if args.crop:
-        dx = args.crop[0]
-        dy = args.crop[1]
-        preview = preview[dx[0]:dx[1], dy[0]:dy[1]]
-
-    # resize image
-    if args.resize:
-        preview = cv2.resize(preview, args.resize, interpolation = cv2.INTER_LINEAR)
+    # Process preview image
+    preview = __process_image(preview, args)
 
     # add data
     BLACK = (0,0,0)
     WHITE = (255,255,255)
     cv2.rectangle(preview,(300,120),(10,10),BLACK,-1)
     cv2.putText(preview, 'Images: %d' % source.size, (20,30), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
-    cv2.putText(preview, 'Crop: %r' % (args.crop), (20,50), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
+    cv2.putText(preview, 'Crop: %r' % [args.crop], (20,50), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
     cv2.putText(preview, 'FPS: %d' % args.fps, (20,70), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
     cv2.putText(preview, 'Duration: %d sec(s)' % (source.size/args.fps), (20,90), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
-    cv2.putText(preview, 'Resolution: %r' % (args.resize), (20,110), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
+    cv2.putText(preview, 'Resolution: %r' % [args.resize], (20,110), cv2.FONT_HERSHEY_SIMPLEX, 0.4, WHITE, 1, cv2.LINE_AA)
 
     # display preview image
     cv2.imshow('pylapse - %s' % source.directory, preview)
@@ -67,16 +74,8 @@ def do_render(source, args):
 
         # Iterate over each image and write to video
         for i, img in enumerate(gen):
-
-            # crop image
-            if args.crop:
-                dx = args.crop[0]
-                dy = args.crop[1]
-                img = img[dx[0]:dx[1], dy[0]:dy[1]]
-
-            # resize image
-            if args.resize:
-                img = cv2.resize(img, args.resize, interpolation = cv2.INTER_LINEAR)
+            # Process image
+            img = __process_image(img, args)
 
             # write image
             out.write(img)
